c79a2c5c8a41a7170c01e794c55975a3572014a7
ivan27ck <aguilarivan908@gmail.com>
2025-03-16 19:20:56 -0600

    add
    puerto cambiado

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,7 @@ import { ConfigModule } from '@nestjs/config';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: 5433,
+      port: 5432,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
